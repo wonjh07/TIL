8afa8a89cd2fedde47319d68c3f31d6f6d9b91c0
wonjh07 <wonjh07@naver.com>
2022-01-14 17:55:18 +0900

    Add startcamp dir

---
+++ startcamp/data_type.py
@@ -0,0 +1,20 @@
+# 숫자
+number = 3
+print(type(number))
+
+# 문자
+string = '문자열입니당~!'
+print(type(string))
+
+#Boolean
+is_25_old = False
+print(type(is_25_old))
+
+#형변환
+string_number = '3'
+result = string_number + 5
+print(result)
+
+first_name = '우석'
+last_name = '김'
+print(first_name + last_name)
